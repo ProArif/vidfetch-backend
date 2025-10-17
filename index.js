2190ca97d14a1975bc5953eb6517bef9bf299b97
Arif <34057291+ProArif@users.noreply.github.com>
2025-10-18 02:56:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
-import { YTDlpWrapExtended } from "yt-dlp-wrap-extended";
+import pkg from "yt-dlp-wrap-extended";
+
+const { YTDlpWrapExtended } = pkg;
 
 const app = express();
 app.use(cors());
